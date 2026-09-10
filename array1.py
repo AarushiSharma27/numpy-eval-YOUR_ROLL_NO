71f58821df810fa3fc69e3e68dba3d2dded9c895
AarushiSharma27 <saarushi725@gmail.com>
2026-09-10 21:31:48 +0530

    Added ML Practical 1

---
+++ array1.py
@@ -0,0 +1,9 @@
+a=[4,7,2,9,1]
+print(a)
+largest = a[0]
+for i in a :
+    if i > largest:
+        largest =i
+print(largest)
+
+
---
+++ array5.py
@@ -0,0 +1,7 @@
+numbers = [-2, 5, -7, 8, 0, 3, -1]
+print(numbers)
+for i in numbers:
+    if i < 0:
+        print("negative no.",numbers[i])
+    else:
+        print("positiv no.",numbers[i])
